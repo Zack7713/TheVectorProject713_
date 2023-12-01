edd619624dc965af5dc1147b828f5529c0bee1f4
CALocander <clocander@yahoo.com>
2023-12-01 18:17:52 -0500

    Added gamemanager and buttonFunctions scripts
    game manager should currently be able to handle all inputs except respawn.

---
+++ Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+
+    public void respawnPlayer()
+    {
+        //gameManager.instance.playerScript.respawnPlayer();
+        gameManager.instance.stateUnpaused();
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System;
+
+public class gameManager : MonoBehaviour
+{
+    public static gameManager instance;
+       
+    [SerializeField] GameObject menuActive;
+    [SerializeField] GameObject menuPause;
+    [SerializeField] GameObject menuWin;
+    [SerializeField] GameObject menuLose;
+
+    public Image playerHPBar;
+
+    [SerializeField] TMP_Text enemyCountText;
+
+    public GameObject player;
+    public playerController playerScript;
+    public GameObject playerSpawnPos;
+
+    public bool isPaused;
+    float timeScaleOriginal;
+    int enemiesRemaining;
+
+    void Awake()
+    {
+        instance = this;
+        player = GameObject.FindWithTag("Player");
+        playerScript = player.GetComponent<playerController>();
+        playerSpawnPos = GameObject.FindWithTag("Player Spawn Pos");
+        timeScaleOriginal = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel") && menuActive == null)
+        {
+            statePaused();
+            menuActive = menuPause;
+            menuActive.SetActive(isPaused);
+        }
+    }
+
+    public void statePaused() 
+    {
+        isPaused = !isPaused;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void stateUnpaused() 
+    {
+        isPaused = !isPaused;
+        Time.timeScale = timeScaleOriginal;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        menuActive.SetActive(false);
+        menuActive = null;
+    }
+
+    public void updateGameGoal(int amount)
+    {
+        enemiesRemaining += amount;
+        enemyCountText.text = enemiesRemaining.ToString("0");
+
+
+        if (enemiesRemaining <= 0)
+        {           
+            statePaused();
+            menuActive = menuWin;
+            menuActive.SetActive(true);
+        }
+    }
+
+    public void youLose()
+    {
+        statePaused();
+        menuActive = menuLose;
+        menuActive.SetActive(true);
+    }
+}
