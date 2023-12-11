ee4650b13196744316899d91b6fed69d045331f2
JesseAdams313 <jsadams@student.fullsail.edu>
2023-12-11 14:24:06 -0800

    Scriptable Object
    Gun - Pistol

---
+++ Assets/Scripts/gunStats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class gunStats : ScriptableObject
+{
+    public int shootDamage;
+    public float shootRate;
+    public int shootDist;
+    public int ammoCur;
+    public int ammoMax;
+
+    public GameObject model;
+    public ParticleSystem hitEffect;
+    public AudioClip shootSound;
+    public float shootSpeed;
+    [Range(0, 1)] public float soundVolume;
+}
