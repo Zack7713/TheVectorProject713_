1fb6781d35a9fcbabc604d0e9b75cc3b0b5dae1d
Zack7713 <126509360+Zack7713@users.noreply.github.com>
2024-01-11 19:18:47 -0500

    working on script for inventory items
    added the script

--- Assets/Scripts/InventoryItems.cs
+++ Assets/Scripts/InventoryItems.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Interact : MonoBehaviour
+[CreateAssetMenu(menuName ="Inventory Item Data")]
+public class inventoryItems :ScriptableObject
 {
+    public string id;
+    public string displayName;
+    public Sprite icon;
+    public GameObject prefab; 
+
+    
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/Shopkeep.cs
+++ Assets/Scripts/Shopkeep.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObjects : MonoBehaviour
+public class Shopkeep : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
