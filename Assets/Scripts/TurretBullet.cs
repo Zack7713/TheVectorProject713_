87ab3185901843c748e999b99f242e89ade4bd64
CALocander <clocander@yahoo.com>
2024-01-15 18:29:00 -0500

    Added functional turrets
    Added 2 turrets (standard and missile). Turrets have ability to track all objects tagged "Enemy". Adjustable ranges and fire rates. Turrets automatically start shooting with the closest target within range.  Added 3 new prefab folders. One for standard turret, one for missile turret and one for turret bullet. projectiles when collided with an enemy have simple particle effects.

---
+++ Assets/Scripts/TurretBullet.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class TurretBullet : MonoBehaviour
+{
+    private Transform target; // What we're shooting at
+
+    [SerializeField] float speed;
+
+    [SerializeField] float explosionRadius;
+
+    [SerializeField] GameObject impactEffect;
+
+    public void Seek(Transform _target)
+    {
+        target = _target;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) 
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 direction = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (direction.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+
+        transform.Translate(direction.normalized * distanceThisFrame, Space.World);
+        transform.LookAt(target);
+    }
+
+    void HitTarget()
+    {
+        // Initializing the particle effect
+        GameObject effectInstance = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effectInstance, 2f);
+
+        if (explosionRadius > 0)
+        {
+            Explode();
+        }
+        else
+        {
+            //Damage the enemy here
+        }
+
+        Destroy(gameObject); // Destroying the bullet prefab
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (Collider collider in colliders)
+        {
+            if(collider.tag == "Enemy")
+            {
+                // Apply Damage to (collider.transform)
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
--- Assets/Scripts/TurretRotation.cs
+++ Assets/Scripts/TurretRotation.cs
@@ -4,18 +4,97 @@ using UnityEngine;
 
 public class TurretRotation : MonoBehaviour
 {
-    public float speed = 10f; // how fast to scan
-    public float angle = 45f; // how much to scan
+    [Header("-----Attributes-----")]
+    [SerializeField] float range;
+    [SerializeField] float fireRate;
+    private float fireCountDown = 0;
+
+    [Header("-----Enemy-----")]
+    [SerializeField] Transform target;
+    [SerializeField] string enemyTag = "Enemy";
+
+    [Header("-----Bullet-----")]
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform firePoint;
+
+    [Header("-----Rotation-----")]
+    [SerializeField] Transform pivot;
+    [SerializeField] float turnSpeed;
+    
+
+    void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
 
     private void Update()
     {
-        // calculate the rotation angle using PingPong
-        float rotation = Mathf.PingPong(Time.time * speed, angle * 2) - angle;
+        if (target == null) 
+        {
+            return;
+        }
 
-        // create a rotation with the specified angle around the y-axis
-        Quaternion rot = Quaternion.Euler(0, rotation, 0);
+        // Target lock on
+        Vector3 direction = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 rotation = Quaternion.Lerp(pivot.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;
+        pivot.rotation = Quaternion.Euler(0f, rotation.y, 0f);
 
-        // apply the rotation to the object
-        transform.rotation = rot;
+        if(fireCountDown <= 0)
+        {
+            Shoot();
+            fireCountDown = 1 / fireRate;
+        }
+
+        fireCountDown -= Time.deltaTime;
+    }
+    void UpdateTarget()
+    {
+        // Puts all enemies with the same tag into array
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+
+        // Calculate the closest enemy to turret location
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (distanceToEnemy < shortestDistance) 
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        // If enemy is within range, assign it as a target
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else 
+            target = null;
+    }
+
+    void Shoot()
+    {
+        // Instantiate the bullet object and get the script from the bullet object
+        GameObject turretBullet = (GameObject)Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        TurretBullet bullet = turretBullet.GetComponent<TurretBullet>();
+
+        // If we made a bullet, call the bullets function
+        if (bullet != null)
+        {
+            bullet.Seek(target);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        // Visually show max range in scene view
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(pivot.position, range);
     }
 }
+
+
