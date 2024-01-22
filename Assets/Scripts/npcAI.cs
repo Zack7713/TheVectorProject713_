35aaffe17a60d43b818724dc0c1ad4811574ec1c
LugoL611 <LELugoRosas@student.fullsail.edu>
2024-01-22 02:19:23 -0500

    Added NPCs
    Added assets, animations and scripts for NPCs

---
+++ Assets/Scripts/npcAI.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class npcAI : MonoBehaviour
+{
+    [Header("----- Components -----")]
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Renderer model;
+    [SerializeField] Transform headPos;
+    [SerializeField] Animator anim;
+    [SerializeField] int viewCone;
+    [SerializeField] int targetFaceSpeed;
+    [SerializeField] float animSpeedTrans;
+    bool playerInRange;
+    bool isWaving;
+    float angleToPlayer;
+    float distanceToPlayer;
+    Vector3 playerDir;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerInRange && canSeePlayer())
+        {
+            distanceToPlayer = Vector3.Distance(transform.position, gameManager.instance.player.transform.position);
+            callPlayer(true);
+            if (distanceToPlayer <= agent.stoppingDistance && isWaving)
+            {
+                callPlayer(false);
+            }
+        }
+        else if (!playerInRange || !canSeePlayer())
+        {
+            if (isWaving)
+            {
+                callPlayer(false);
+            }
+        }
+    }
+    bool canSeePlayer()
+    {
+        playerDir = gameManager.instance.player.transform.position - headPos.position;
+        angleToPlayer = Vector3.Angle(playerDir, transform.forward);
+
+        Debug.DrawRay(headPos.position, playerDir);
+        Debug.Log(angleToPlayer);
+
+        RaycastHit hit;
+        if (Physics.Raycast(headPos.position, playerDir, out hit))
+        {
+            if (hit.collider.CompareTag("Player") && angleToPlayer <= viewCone)
+            {
+                faceTarget();
+                return true;
+            }
+        }
+        return false;
+    }
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * targetFaceSpeed);
+    }
+    public void callPlayer(bool wave)
+    {
+        isWaving = wave;
+        anim.SetBool("Call", isWaving);
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
