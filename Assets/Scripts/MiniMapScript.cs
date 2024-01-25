7b85740b6609069e471eb5af4726056fd297bb2b
JesseAdams313 <jsadams@student.fullsail.edu>
2024-01-25 09:35:34 -0800

    Mini Map
    Mini Map needs to be masked

---
+++ Assets/Scripts/MiniMapScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MiniMapScript : MonoBehaviour
+{
+    public Transform player;
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+     
+}
